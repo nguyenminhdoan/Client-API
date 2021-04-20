01d72fd39a8df4d31da56370fa1da5e390b080f9
nguyenminhdoan <50704169+nguyenminhdoan@users.noreply.github.com>
2021-04-20 12:29:33 +0700

    get all tickets api added by specific user

--- model/ticket/Ticket.model.js
+++ model/ticket/Ticket.model.js
@@ -13,4 +13,17 @@ const insertTicket = (ticketObj) => {
   });
 };
 
-module.exports = { insertTicket };
+// GET ALL TICKET
+const getTickets = (clientId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.find({ clientId })
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = { insertTicket, getTickets };
--- src/routers/ticket.router.js
+++ src/routers/ticket.router.js
@@ -1,34 +1,62 @@
 const express = require("express");
 const router = express.Router();
 
-const { insertTicket } = require("../../model/ticket/Ticket.model");
+const {
+  userAuthorization,
+} = require("../../middlewares/authorization.middleware");
+
+const { insertTicket, getTickets } = require("../../model/ticket/Ticket.model");
 
 router.all("/", (req, res, next) => {
   next();
 });
 
-router.post("/", async (req, res, next) => {
-  const { subject, sender, message } = req.body;
-  // insert data to mongoDB
-  const ticketObj = {
-    clientId: "606b2433d4c554175461dc8d",
-    subject: subject,
-    conversation: [
-      {
-        sender: sender,
-        message: message,
-      },
-    ],
-  };
+// create new ticket
+router.post("/", userAuthorization, async (req, res, next) => {
+  try {
+    const { subject, sender, message } = req.body;
+    const userId = req.userId;
+    // insert data to mongoDB
+    const ticketObj = {
+      clientId: userId,
+      subject: subject,
+      conversation: [
+        {
+          sender: sender,
+          message: message,
+        },
+      ],
+    };
 
-  const result = await insertTicket(ticketObj);
+    const result = await insertTicket(ticketObj);
 
-  if (result._id) {
-    return res.json({
-      status: "success",
-      message: "new ticket has been created",
-    });
+    if (result._id) {
+      return res.json({
+        status: "success",
+        message: "new ticket has been created",
+      });
+    }
+    res.json({ status: "error", message: "error occurs, please try later!!!" });
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
   }
-  res.json({ status: "error", message: "error occurs, please try later!!!" });
+});
+
+// GET ALL tickets
+router.get("/", userAuthorization, async (req, res, next) => {
+  try {
+    const userId = req.userId;
+    console.log(userId);
+    const result = await getTickets(userId);
+
+    if (result.length) {
+      return res.json({
+        status: "success",
+        result,
+      });
+    }
+  } catch (error) {
+    res.json({ status: "error", message: error.message });
+  }
 });
 module.exports = router;
